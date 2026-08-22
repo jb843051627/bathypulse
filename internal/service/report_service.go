59267d2a2f851c76eab0150b21db6b2881a8b18c
jiabo <843051627@qq.com>
2026-08-22 13:39:23 +0800

    feat: update bathypulse logic

--- internal/service/report_service.go
+++ internal/service/report_service.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (s *ObservatoryService) Summary(ctx context.Context) (model.ObservatorySummary, error) {
-	summary, err := s.queries.Summary(context.Background())
+	summary, err := s.queries.Summary(ctx)
 	if err != nil {
 		return summary, err
 	}
